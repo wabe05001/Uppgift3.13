f56fe18b1bd6287f19b014868a7404aa3731c161
BaoLanNguyen <89245290+BaoLanNguyen@users.noreply.github.com>
2022-09-30 10:20:45 +0200

    Add project files.

---
+++ Uppgift3.13/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Uppgift_3._13
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("är du över 16 år gammal?svara i antal år");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if(ålder >=19)
+            {
+                Console.WriteLine("du får delta i tävlingen");
+
+            }
+
+            if(ålder >=16)
+            {
+                Console.WriteLine("du får oxå vara med i tävlingen");
+
+            }
+
+            if(ålder <=16)
+            {
+                Console.WriteLine("du får tyvärr inte vara med i tävlingen");
+
+            }
+
+
+
+
+        }
+
+    }
+
+
+}
